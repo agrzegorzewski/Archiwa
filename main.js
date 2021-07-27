014c1dd24d8c46c1af2549d62f6b9b7f89d12be9
Turipuru-chan <triplemonkey47@gmail.com>
2021-07-27 13:16:02 +0200

    Added date option to select date(broken rn), revised some code

--- main.js
+++ main.js
@@ -1,12 +1,66 @@
 function sendRequest(un, pw) {
-    let token = "Basic " + btoa(un + ":" + pw);
-    console.log(token);
+    const token = "Basic " + btoa(un + ":" + pw);
 
-    var myHeaders = new Headers();
+    const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Authorization", token);
 
-    var raw = JSON.stringify({
+    const requestParameters = {
+        method: 'POST',
+        headers: myHeaders,
+        body: createBody(),
+        redirect: 'follow'
+    };
+
+    makeCall(requestParameters);
+
+}
+
+function setLeftChats(result) {
+    document.getElementById('chats_left').value = "Chats left to tag: " + ((JSON.parse(result).found_chats - JSON.parse(result).chats.length) > 0 ? JSON.parse(result).found_chats - JSON.parse(result).chats.length : "0");
+}
+
+function createChatList(result) {
+    document.getElementById('chats_list').innerHTML = "";
+    for (let chatIndex in JSON.parse(result).chats) {
+        let id = JSON.parse(result).chats[chatIndex].thread.id;
+        document.getElementById('chats_list').innerHTML += '<a href="https://my.staging.livechatinc.com/archives/' + id + '" target="_blank">' + id + '</a><br>';
+    }
+}
+
+function makeCall(requestOptions) {
+    fetch("https://api.livechatinc.com/v3.3/agent/action/list_archives", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            setLeftChats(result);
+            createChatList(result);
+        })
+        .catch(error => console.log('error', error));
+}
+
+function addDateToBody(date_from_input, date_to_input, requestBody) {
+
+    const date_from = new Date(date_from_input);
+    const date_to = new Date(date_to_input);
+
+    if (Boolean(date_from_input) !== Boolean(date_to_input)) {
+        return alert('Only one date included.');
+    }
+    if (date_from >= date_to) {
+        return alert('"Date from" is bigger than or equal "date to".')
+    }
+    if (!date_from_input) {
+        return requestBody;
+    }
+
+    requestBody.filters.from = date_from;
+    requestBody.filters.to = date_to;
+
+    return requestBody;
+}
+
+function createBody() {
+    let raw = {
         "limit": 100,
         "filters": {
             "tags": {
@@ -19,39 +73,13 @@ function sendRequest(un, pw) {
             },
             "agents": {
                 "values": [
-                    ""
+                    "agentEmail"
                 ]
             },
         }
-    });
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw.replace('""', `"${document.getElementById('email').value}"`),
-        redirect: 'follow'
     };
 
-    const call = fetch("https://api.livechatinc.com/v3.3/agent/action/list_archives", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            // console.log(JSON.parse(result));
-            setLeftChats(result);
-            createChatList(result);
-            // console.log('found chats ' + JSON.parse(result).found_chats);
-            // console.log('chats length ' + JSON.parse(result).chats.length);
-        })
-        .catch(error => console.log('error', error));
+    raw.filters.agents.values[0] = document.getElementById('email').value;
 
-    function setLeftChats(result) {
-        document.getElementById('chats_left').value = "Chats left to tag: " + ((JSON.parse(result).found_chats - JSON.parse(result).chats.length) > 0 ? JSON.parse(result).found_chats - JSON.parse(result).chats.length : "0");
-    }
-
-    function createChatList(result) {
-        document.getElementById('chats_list').innerHTML = "";
-        for (let i = 0; i < JSON.parse(result).chats.length; i++) {
-            let id = JSON.parse(result).chats[i].thread.id;
-            document.getElementById('chats_list').innerHTML += '<a href="https://my.staging.livechatinc.com/archives/' + id + '" target="_blank">' + id + '</a><br>';
-        }
-    }
+    return JSON.stringify(addDateToBody(document.getElementById('date_from').value, document.getElementById('date_to').value, raw));
 }
